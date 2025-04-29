c4b511cf2eb22e2d0cb57276d8e6a4e4a2696895
mgraphic <mgraphic@users.noreply.github.com>
2025-04-28 22:08:03 -0400

    CMO-38 Cleanup

--- Apps/shared/jest.config 3.js
+++
@@ -1,7 +0,0 @@
-import { createDefaultPreset } from 'ts-jest';
-
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
-export default {
-    ...createDefaultPreset(),
-    preset: 'ts-jest/presets/default-esm',
-};
