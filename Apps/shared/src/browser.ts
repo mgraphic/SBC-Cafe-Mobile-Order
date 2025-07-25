2ebef7ac0d43dc69decab492a2fe9bde5d3db989
mgraphic <mgraphic@users.noreply.github.com>
2025-07-24 22:55:17 -0400

    Using copilot to fix docker build issues

--- Apps/shared/src/browser.ts
+++ Apps/shared/src/browser.ts
@@ -1,29 +1,32 @@
-export { IPageable, IPageableMetadata, PaginatedPayload } from './aws';
-export { ITracker, UserTrackerLogsLookup } from './tracker';
+export type { IPageable, IPageableMetadata, PaginatedPayload } from './aws';
+export type { ITracker, UserTrackerLogsLookup } from './tracker';
 export {
     ACCESS_TOKEN_EXPIRY,
-    AuthUser,
-    getUserPermissionGroupFromPermission,
-    isPermission,
-    isPermissionGroup,
-    isRoleInPermissionGroup,
-    IUser,
+    REFRESH_TOKEN_EXPIRY,
     JWT_CLEARCOOKIE_OPTIONS,
     JWT_COOKIE_EXPIRY,
     JWT_COOKIE_OPTIONS,
+} from './user/jwt.config';
+export {
+    type IUser,
     jwtPayloadFields,
-    JwtUserPayload,
+    type JwtUserPayload,
     rbacPermissionGroups,
     rbacPermissions,
     rbacRoleHiarchyMap,
     rbacRolePermissionGroupAllowances,
     rbacRoles,
-    REFRESH_TOKEN_EXPIRY,
-    roleCompare,
-    TokenUser,
-    UserPermission,
-    UserPermissionGroup,
-    UserRole,
+    type UserPermission,
+    type UserPermissionGroup,
+    type UserRole,
     userRoles,
-} from './user';
+} from './user/user.model';
+export {
+    getUserPermissionGroupFromPermission,
+    isPermission,
+    isPermissionGroup,
+    isRoleInPermissionGroup,
+    roleCompare,
+} from './user/user.utils';
+export { TokenUser } from './user/token-user';
 export { Validator } from './validator';
--- Apps/shared/tsup.config.ts
+++ Apps/shared/tsup.config.ts
@@ -1,11 +1,26 @@
 import { defineConfig } from 'tsup';
 
-export default defineConfig({
-    format: ['cjs', 'esm'],
-    entry: ['./src/index.ts'],
-    dts: true,
-    shims: true,
-    skipNodeModulesBundle: true,
-    sourcemap: true,
-    clean: true,
-});
+export default defineConfig([
+    // Node/Server build
+    {
+        format: ['cjs', 'esm'],
+        entry: ['./src/index.ts'],
+        dts: true,
+        shims: true,
+        skipNodeModulesBundle: true,
+        sourcemap: true,
+        clean: true,
+    },
+    // Browser build - including JS implementation
+    {
+        format: ['esm'],
+        entry: ['./src/browser.ts'],
+        dts: true,
+        outDir: 'dist',
+        outExtension: () => ({ js: '.browser.js', dts: '.browser.d.ts' }),
+        skipNodeModulesBundle: true,
+        sourcemap: true,
+        clean: false,
+        minify: true,
+    },
+]);
